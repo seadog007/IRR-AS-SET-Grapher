f5e466dee9570e7907d44b6250871a2e981b1e97
Jasper Yu <007seadog@gmail.com>
2021-03-18 16:15:38 +0800

    Added graph basic

---
+++ graph.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function err(){
+	echo "$1" >&2
+}
+
+function asset(){
+	whois "$1" | grep -oP 'members:\ +\KAS.*' | while read line
+	do
+		echo "$(echo "$1" | sed 's/[-:]/_/g')->$(echo "$line" | sed 's/[-:]/_/g')"
+		[ -n "$(echo $2 | grep "$1")" ] && err "$1 Abort" && continue
+		"$0" "$line" "$2 $1"
+	done
+
+}
+
+function as(){
+	err "$1"
+}
+
+[ -z "$1" ] && echo 'Please Input Inital AS-Set' && exit 1
+
+[ -z "$(echo "$1" | grep '^AS[0-9\-]')" ] && err "Not an AS/AS-SET" && exit 1
+[ -n "$(echo "$1" | grep '^AS-.*$')" ] || [ -n "$(echo "$1" | grep '^AS[0-9]\+:.*$')" ] && err "$1 AS-SET" && asset "$1" "$2"
+[ -n "$(echo "$1" | grep '^AS[0-9]\+$')" ] && err "$1 Normal AS" && as "$1" 
