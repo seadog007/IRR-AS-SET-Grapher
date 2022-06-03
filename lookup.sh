d7fd28d7605cfd230982b32c29b63ef8605cd9af
Jasper Yu <007seadog@gmail.com>
2022-06-03 10:26:15 +0800

    Bugfix: Added delimiter to as-set list When situation like the example below, it might not processing completely AS18041:AS-18041-IPT -> AS-18041-IPT (Stop here is incorrect) -> AS131668 The call stack will be ./graph.sh "AS18041:AS-18041-IPT" "" ./graph.sh "AS-18041-IPT" " AS18041:AS-18041-IPT" -> end here (since "AS-18041-IPT" appear in " AS18041:AS-18041-IPT")
    After the mitigration, the callstack will be
    ./graph.sh "AS18041:AS-18041-IPT" ""
    ./graph.sh "AS-18041-IPT" " @AS18041:AS-18041-IPT@"
    -> "@AS-18041-IPT@" not in " @AS18041:AS-18041-IPT@", continue
    ./graph.sh "AS131668" "@AS18041:AS-18041-IPT@ @AS-18041-IPT@"

--- lookup.sh
+++ lookup.sh
@@ -8,16 +8,14 @@ function asset(){
 	whois -m "$1" | grep -oP 'members:\ +\KAS.*' | tr ',' '\n' | tr -d ' ' | while read line
 	do
 		echo '"'"$1"'"->"'"$line"'"'
-		[ -n "$(echo $2 | grep "$1")" ] && err "$1 Abort" && continue
-		"$0" "$line" "$2 $1"
+		[ -n "$(echo $2 | grep "@$1@")" ] && err "$1 Processed, Aborting" && continue
+		"$0" "$line" "$2 @$1@"
 	done
-
 }
 
 function as(){
 	err "$1"
 }
-
 
 [ -z "$(echo "$1" | grep '^AS[0-9\-]')" ] && err "Not an AS/AS-SET" && exit 1
 [ -n "$(echo "$1" | grep '^AS-.*$')" ] || [ -n "$(echo "$1" | grep '^AS[0-9]\+:.*$')" ] && err "$1 AS-SET" && asset "$1" "$2"
